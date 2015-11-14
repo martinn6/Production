2e66c42cb56afd8a1565a8a48fb5ce1fcc40d525
nickdmartin <nickdmartin@outlook.com>
2015-11-14 14:39:36 -0800

    Add Production

---
+++ Production.js
@@ -0,0 +1,76 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 80);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/',function(req,res){
+  res.render('home')
+});
+
+app.get('/show-data',function(req,res){
+  var context = {};
+  context.sentData = req.query.myData;
+  res.render('show-data', context);
+});
+
+app.get('/get-loopback',function(req,res){
+  var qParams = "";
+  for (var p in req.query){
+    qParams += "The name " + p + " contains the value " + req.query[p] + ", ";
+  }
+  qParams = qParams.substring(0,qParams.lastIndexOf(','));
+  qParams += '.';
+  var context = {};
+  context.dataList = qParams;
+  res.render('get-loopback', context);
+});
+
+app.get('/getpost',function(req,res){
+  var qParams = [];
+  for (var p in req.query){
+    qParams.push({'name':p,'value':req.query[p]})
+  }
+  var context = {};
+  context.dataList = qParams;
+  res.render('getresponse', context);
+});
+
+app.post('/getpost', function(req,res){
+  var qParams = [];
+  for (var p in req.body){
+    qParams.push({'name':p,'value':req.body[p]})
+  }
+  console.log(qParams);
+  console.log(req.body);
+  var context = {};
+  context.dataList = qParams;
+  res.render('postresponse', context);
+});
+
+app.get('/other-page',function(req,res){
+  res.render('other-page');
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+ 
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
