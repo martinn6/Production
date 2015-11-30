a011037e5586c22e82876bcb997aa822a8a0813a
nickdmartin <nickdmartin@outlook.com>
2015-11-29 16:55:11 -0800

    test

--- Production.js
+++ Production.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 app.use('/production/images', express.static('public'));
 
 
