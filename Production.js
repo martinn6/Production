29f8d4b5d6bc4bd9edc26d84ce03519bc22c9912
nickdmartin <nickdmartin@outlook.com>
2015-12-06 00:07:09 -0800

    adsf

--- Production.js
+++ Production.js
@@ -26,12 +26,12 @@ app.get('/reset-table',function(req,res,next){
   var context = {};
   mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
     var createString = "CREATE TABLE workouts("+
-    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    //"id INT PRIMARY KEY AUTO_INCREMENT,"+
     "name VARCHAR(255) NOT NULL,"+
-    "reps INT,"+
-    "weight INT,"+
-    "date DATE,"+
-    "lbs BOOLEAN)";
+    //"reps INT,"+
+   // "weight INT,"+
+   // "date DATE,"+
+   // "lbs BOOLEAN)";
     mysql.pool.query(createString, function(err){
       context.results = "Table reset";
       res.render('reset-table',context);
@@ -70,7 +70,7 @@ app.get('/workout',function(req,res,next){
   });
 });
 
-
+/*
 app.post('/workout',function(req,res,next){
   var context = {};
   
@@ -96,7 +96,7 @@ app.post('/workout',function(req,res,next){
     res.render('workout', context);
   });
 });
-
+*/
 
 
 
