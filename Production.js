1d520b1fcff033b84a9219823fdc342db2c6ecd1
nickdmartin <nickdmartin@outlook.com>
2015-12-05 20:30:11 -0800

    asdf

--- Production.js
+++ Production.js
@@ -22,6 +22,24 @@ function getRandomNum() {
 	return stuff;
 }
 
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  [your connection pool].query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+    var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
+    [your connection pool].query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    })
+  });
+});
+
+
 app.get('/',function(req,res){
   res.render('home')
 });
