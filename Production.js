10b8068d52a4c66b17c3521392d051e4b78c17e9
nickdmartin <nickdmartin@outlook.com>
2015-12-06 01:12:49 -0800

    test

--- Production.js
+++ Production.js
@@ -114,7 +114,7 @@ app.post('/workout',function(req,res,next){
 
 app.post('/workoutupdate',function(req,res){
   var list = [{"id":""},{"name":""}];
-  var context = {new list};
+  var context = {[list]};
   
   context.list.id = [req.body.id];
   context.list.name = [req.body.name];
